b72f1fdd365150c13dfa93f6fd31e10bfd232cdb
AnhNH <anhnh11032001@gmail.com>
2023-11-11 23:14:10 +0700

    init project

---
+++ src/main/java/com/ic/searchserver/SearchserverApplication.java
@@ -0,0 +1,13 @@
+package com.ic.searchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchserverApplication.class, args);
+	}
+
+}
